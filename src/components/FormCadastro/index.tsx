a7c27bed14b23fa733b751f4ca78c013a2148eb7
jean carllo <jeanrosete04@gmail.com>
2024-09-17 11:56:10 -0300

    alteração de exibição de tabela

--- src/components/FormCadastro/index.tsx
+++ src/components/FormCadastro/index.tsx
@@ -2,9 +2,20 @@ import React, { useState } from 'react'
 import { ContentButton, ContentForm } from './style'
 import useClientes from '../../hooks/useClientes'
 import { Cliente } from '../../types'
+import TabelaConsulta from '../TabelaConsulta'
+// import Header from '../Header'
 
 const FormCadastro = () => {
   const { addCliente } = useClientes()
+  const [exibirTabela, setExibirTabela] = useState(false)
+
+  const handleToggeTabela = () => {
+    setExibirTabela(!exibirTabela)
+  }
+
+  const handleBack = () => {
+    setExibirTabela(false)
+  }
 
   const [cliente, setCliente] = useState<Cliente>({
     id: 0,
@@ -35,93 +46,105 @@ const FormCadastro = () => {
   }
 
   return (
-    <ContentForm>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="nome">Nome</label>
-        <input
-          id="nome"
-          type="text"
-          placeholder="Nome"
-          value={cliente.nome}
-          onChange={onChange}
-          required
-        />
-        <label htmlFor="sobreNome">Sobre Nome</label>
-        <input
-          id="sobreNome"
-          type="text"
-          placeholder="Sobre Nome"
-          value={cliente.sobreNome}
-          onChange={onChange}
-          required
-        />
-        <label htmlFor="email"> E-mail</label>
-        <input
-          id="email"
-          type="email"
-          placeholder="E-mail"
-          value={cliente.email}
-          onChange={onChange}
-          required
-        />
-        <label htmlFor="idade">Idade</label>
-        <input
-          id="idade"
-          type="number"
-          placeholder="Idade"
-          value={cliente.idade}
-          onChange={onChange}
-          required
-        />
-        <label htmlFor="endereco">Endereço</label>
-        <input
-          id="endereco"
-          type="text"
-          placeholder="Endereço"
-          value={cliente.endereco}
-          onChange={onChange}
-          required
-        />
-        <label htmlFor="cidade">Cidade</label>
-        <input
-          id="cidade"
-          type="text"
-          placeholder="Cidade"
-          value={cliente.cidade}
-          onChange={onChange}
-          required
-        />
-        <label htmlFor="estado">Estado</label>
-        <input
-          id="estado"
-          type="text"
-          placeholder="Estado"
-          value={cliente.estado}
-          onChange={onChange}
-          required
-        />
-        <ContentButton>
-          <button type="submit">Salvar</button>
-          <button
-            type="button"
-            onClick={() =>
-              setCliente({
-                id: 0,
-                nome: '',
-                sobreNome: '',
-                email: '',
-                idade: 0,
-                endereco: '',
-                cidade: '',
-                estado: ''
-              })
-            }
-          >
-            Limpar
-          </button>
-        </ContentButton>
-      </form>
-    </ContentForm>
+    <>
+      <header>
+        <h1>Dados Pessoais</h1>
+      </header>
+      <ContentForm>
+        {exibirTabela ? (
+          <TabelaConsulta onBack={handleBack} />
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="nome">Nome</label>
+            <input
+              id="nome"
+              type="text"
+              placeholder="Nome"
+              value={cliente.nome}
+              onChange={onChange}
+              required
+            />
+            <label htmlFor="sobreNome">Sobre Nome</label>
+            <input
+              id="sobreNome"
+              type="text"
+              placeholder="Sobre Nome"
+              value={cliente.sobreNome}
+              onChange={onChange}
+              required
+            />
+            <label htmlFor="email"> E-mail</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="E-mail"
+              value={cliente.email}
+              onChange={onChange}
+              required
+            />
+            <label htmlFor="idade">Idade</label>
+            <input
+              id="idade"
+              type="number"
+              placeholder="Idade"
+              value={cliente.idade}
+              onChange={onChange}
+              required
+            />
+            <label htmlFor="endereco">Endereço</label>
+            <input
+              id="endereco"
+              type="text"
+              placeholder="Endereço"
+              value={cliente.endereco}
+              onChange={onChange}
+              required
+            />
+            <label htmlFor="cidade">Cidade</label>
+            <input
+              id="cidade"
+              type="text"
+              placeholder="Cidade"
+              value={cliente.cidade}
+              onChange={onChange}
+              required
+            />
+            <label htmlFor="estado">Estado</label>
+            <input
+              id="estado"
+              type="text"
+              placeholder="Estado"
+              value={cliente.estado}
+              onChange={onChange}
+              required
+            />
+            <ContentButton>
+              <button type="submit">Salvar</button>
+              <button
+                type="button"
+                onClick={() =>
+                  setCliente({
+                    id: 0,
+                    nome: '',
+                    sobreNome: '',
+                    email: '',
+                    idade: 0,
+                    endereco: '',
+                    cidade: '',
+                    estado: ''
+                  })
+                }
+              >
+                Limpar
+              </button>
+              <button type="button" onClick={handleToggeTabela}>
+                Exibir Tabela
+              </button>
+            </ContentButton>
+          </form>
+        )}
+      </ContentForm>
+    </>
   )
 }
 
